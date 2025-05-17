de3eefefb311fad46574e6c1c19dcdd4dee35aa9
Papacu <viniciussadxxx@gmail.com>
2025-05-17 18:43:09 -0300

    switch trebleapp

--- TrebleApp/Android.mk
+++ TrebleApp/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := TrebleApp
-LOCAL_SRC_FILES := app.apk
+LOCAL_SRC_FILES := ../../../treble_app/TrebleApp.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_BUILT_MODULE_STEM := package.apk
