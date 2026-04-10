a71a9c25837d6dcb23389629dafa1c670f72dcff
rolexf-panel <jundibabaji@gmail.com>
2026-04-10 22:10:00 +0800

    Add TECNO LJ6 overlay with 7000mAh battery (fixed format)

--- overlay.mk
+++ overlay.mk
@@ -347,6 +347,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-tecno-pova5pro \
 	treble-overlay-tecno-pova5pro-systemui \
 	treble-overlay-tecno-pova7ultra \
+	treble-overlay-tecno-lj6 \
 	treble-overlay-tecno-spark10pro \
 	treble-overlay-tecno-spark5 \
 	treble-overlay-tecno-spark6 \
